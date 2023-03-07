3a5ad248a3fd91309ee28249461e23be075e160a
LabdhPurohit <80207914+LabdhPurohit@users.noreply.github.com>
2023-03-07 15:57:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<!doctype html>
-<html lang="en">
-  <head>
-  	<title>Barkha Weds Mridul</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<link rel="stylesheet" href="css/style.css">
-
-	</head>
-	<body class="img js-fullheight" style="background-image: url(https://picsum.photos/1920/1080/?blur=5);">
 		 <?php
     session_start();
 
@@ -39,6 +24,23 @@
     }
 
     ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Barkha Weds Mridul</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body class="img js-fullheight" style="background-image: url(https://picsum.photos/1920/1080/?blur=5);">
+
 	<section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
